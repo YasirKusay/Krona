96a1bb9419c827ff632d7349ba8ef3a253b4b0a9
YasirKusay <yasirsaad1234@hotmail.com>
2023-04-06 12:33:52 +1000

    added a feature that allows you to see average query length

--- KronaTools/src/krona-2.0.js
+++ KronaTools/src/krona-2.0.js
@@ -5542,7 +5542,7 @@ function mouseClick(e)
 				        document.getElementById("text_display").remove();
 				    }
                                     toInsert.style.cssText = 'position:absolute;bottom: 0;left: 0; height: 100px; width: 150px;opacity:1;z-index:100;';
-                                    toInsert.innerHTML = highlightedNode.name.bold() + "<br/>" + "\nE-val: ".bold() + curr[1] + "<br/>" + "Bitscore: ".bold() + curr[2] + "<br/>" + "\n% ID: ".bold() + curr[3];
+                                    toInsert.innerHTML = highlightedNode.name.bold() + "<br/>" + "\nE-val: ".bold() + curr[1] + "<br/>" + "Bitscore: ".bold() + curr[2] + "<br/>" + "\n% ID: ".bold() + curr[3] + "<br/>" + "Avg Query Length: ".bold() + curr[4];
                                 document.body.appendChild(toInsert);
                                 }
 				//console.log(curr[0]);
